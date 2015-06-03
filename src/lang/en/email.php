94497ed225e0add5a394c43fb1821791333f56f2
Rolly Maulana Awangga <rolly@awang.ga>
2015-06-03 22:19:00 +0700

    add en language message

---
+++ src/lang/en/email.php
@@ -0,0 +1,5 @@
+<?php
+
+return array(
+	'title' => 'Please confirm your account',
+);
---
+++ src/lang/en/messages.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+	'auth'	=> "You must be logged in",
+	'error' => "An error occured. Please try again later",
+	'success' => "A recovery email was sent to your inbox",
+	'confirmed' => 'Your email is confirmed',
+	'email-not-found' => 'Unknown email',
+	'token-not-found' => 'Your token is invalid or expired',
+);
